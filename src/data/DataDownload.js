cc2223015cbbb7d334c1dc5c5f578536f4960143
benjamin sarrazin <sarrazin.benjamin.pro@gmail.com>
2023-05-02 09:19:25 +0200

    fix responsive card

--- src/data/DataDownload.js
+++ src/data/DataDownload.js
@@ -8,14 +8,14 @@ export const DataDownload = [
     title: "IOS",
     url_img: "svg2.svg",
     containerClass:
-      "h-[500px] gap-y-5 md:h-[600px] overflow-hidden md:col-start-1 col-end-2 row-start-1 row-end-2",
+      "h-[600px]  md:h-[600px] overflow-hidden md:col-start-1 col-end-2 row-start-1 row-end-2",
   },
   {
     id: 2,
     title: "Android",
     url_img: "svg3.svg",
     containerClass:
-      "h-[500px] gap-y-5 md:h-[600px] overflow-hidden lg:col-start-2 col-end-2 row-start-2 row-end-2 md:col-start-2 col-end-2 row-start-2 row-end-2",
+      "aboslute h-[600px] -mt-44 md:h-[600px] overflow-hidden lg:col-start-2 col-end-2 row-start-2 row-end-2 md:col-start-2 col-end-2 row-start-2 row-end-2",
   },
   {
     id: 3,
@@ -23,14 +23,14 @@ export const DataDownload = [
     url_img: "svg5.svg",
     className: `${styleClasse}`,
     containerClass:
-      "h-[500px] gap-y-5 md:h-[600px] overflow-hidden lg:col-start-1 col-end-2 row-start-3 row-end-4 md:col-start-1 col-end-2 row-start-3 row-end-4 relative -mt-48",
+      "h-[500px] md:h-[600px] overflow-hidden lg:col-start-1 col-end-2 row-start-3 row-end-4 md:col-start-1 col-end-2 row-start-3 row-end-4 relative -mt-48",
   },
   {
     id: 4,
     title: "Mac",
     url_img: "svg6.svg",
     containerClass:
-      "h-[500px] gap-y-5 md:h-[600px] overflow-hidden lg:col-start-2 col-end-3 row-start-4 row-end-5 md:col-start-2 col-end-3 row-start-4 row-end-5 relative -mt-48 2xl:-mt-96",
+      "h-[500px] md:h-[600px] overflow-hidden lg:col-start-2 col-end-3 row-start-4 row-end-5 md:col-start-2 col-end-3 row-start-4 row-end-5 relative -mt-48 2xl:-mt-96",
   },
   {
     id: 5,
@@ -38,6 +38,6 @@ export const DataDownload = [
     content:
       "Essaie notre version bêta publique et teste de nouvelles fonctionnalités avant leur lancement.",
     containerClass:
-      "h-[500px] gap-y-5 md:h-[600px] overflow-hidden lg:col-start-1 col-end-2 row-start-5 row-end-5 md:lg:col-start-1 col-end-2 row-start-5 row-end-5 relative -mt-48 ",
+      "h-[500px] md:h-[600px] overflow-hidden lg:col-start-1 col-end-2 row-start-5 row-end-5 md:lg:col-start-1 col-end-2 row-start-5 row-end-5 relative -mt-48 ",
   },
 ];
